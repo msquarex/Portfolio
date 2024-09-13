9403c5515a79f5947e23dc2c3d216554ec8f29e1
msquarex <mittal.madhav12@gmail.com>
2024-09-13 05:42:43 +0530

    bg errors

--- app/components/Background3D.tsx
+++ app/components/Background3D.tsx
@@ -1,20 +1,23 @@
-import { useRef} from 'react'
+import { useRef } from 'react'
 import { Canvas, useFrame } from '@react-three/fiber'
 import { Points, PointMaterial } from '@react-three/drei'
 import * as random from 'maath/random/dist/maath-random.esm'
+import * as THREE from 'three';
 
-function Stars(props: any) {
-  const ref = useRef<any>()
+function Stars() {
+  const ref = useRef<THREE.Points>(null!)
   const sphere = random.inSphere(new Float32Array(3000), { radius: 1.5 })
 
   useFrame((state, delta) => {
-    ref.current.rotation.x -= delta / 10
-    ref.current.rotation.y -= delta / 15
+    if (ref.current) {
+      ref.current.rotation.x -= delta / 10
+      ref.current.rotation.y -= delta / 15
+    }
   })
 
   return (
     <group rotation={[0, 0, Math.PI / 4]}>
-      <Points ref={ref} positions={sphere} stride={3} frustumCulled {...props}>
+      <Points ref={ref} positions={sphere} stride={3} frustumCulled>
         <PointMaterial
           transparent
           color="#ffffff"
